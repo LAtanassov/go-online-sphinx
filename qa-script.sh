68964e68e5fecce37ae870e5384b69397c6920fb
latanassov <latschesar.atanassov@gmx.at>
2018-10-21 13:39:53 +0200

    rm & ignore generated report update qa-script

--- qa-script.sh
+++ qa-script.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+mkdir -p report
+
 # race detection
-go test ./... -race
+go test ./... -race 2>&1 | tee ./report/race.txt
 
 # coverage
-go test ./... -coverprofile=./report/coverage/coverage.out 2>&1 > ./report/test/test.txt
-go tool cover -html=./report/coverage/coverage.out -o ./report/coverage/coverage.html
+go test ./... -coverprofile=./report/coverage.out
+go tool cover -html=./report/coverage.out -o ./report/coverage.html
 
 # allocation
-go build -gcflags "-m -m" ./cmd/osctl/... 2>&1 | tee ./report/allocation/osctl.txt
-go build -gcflags "-m -m" ./cmd/ossrv/... 2>&1 | tee ./report/allocation/ossrv.txt
+go build -gcflags "-m -m" ./cmd/osctl/... 2>&1 | tee ./report/allocation_osctl.txt
+go build -gcflags "-m -m" ./cmd/ossrv/... 2>&1 | tee ./report/allocation_ossrv.txt
