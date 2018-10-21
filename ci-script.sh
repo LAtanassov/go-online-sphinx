73a8a2ef2edb01c989efd78c511728dcf5d2aa34
latanassov <latschesar.atanassov@gmx.at>
2018-10-21 11:33:30 +0200

    seperate ci, qa update README

--- ci-script.sh
+++ ci-script.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-# test and coverage
-go test ./... -coverprofile=./report/coverage/coverage.out 2>&1 > ./report/test/test.txt
-go tool cover -html=./report/coverage/coverage.out -o ./report/coverage/coverage.html
-
-# allocation
-go build -gcflags "-m -m" ./cmd/osctl/... 2>&1 | tee ./report/allocation/osctl.txt
-go build -gcflags "-m -m" ./cmd/ossrv/... 2>&1 | tee ./report/allocation/ossrv.txt
+go test ./...
+go vet ./...
 
 go build -tags netgo ./cmd/osctl/...
 go build -tags netgo ./cmd/ossrv/...
---
+++ qa-script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# race detection
+go test ./... -race
+
+# coverage
+go test ./... -coverprofile=./report/coverage/coverage.out 2>&1 > ./report/test/test.txt
+go tool cover -html=./report/coverage/coverage.out -o ./report/coverage/coverage.html
+
+# allocation
+go build -gcflags "-m -m" ./cmd/osctl/... 2>&1 | tee ./report/allocation/osctl.txt
+go build -gcflags "-m -m" ./cmd/ossrv/... 2>&1 | tee ./report/allocation/ossrv.txt
