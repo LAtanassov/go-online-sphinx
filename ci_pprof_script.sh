474da840d80bc56b52ee21fc460e17e1ca9739a7
latanassov <latschesar.atanassov@gmx.at>
2018-11-24 10:27:13 +0100

    upgrade pprof to https

--- ci_pprof_script.sh
+++ ci_pprof_script.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+CUR_DIR=$(pwd)
+
+if [ ! -f $CUR_DIR/certs/server.key ]; then
+    cd $CUR_DIR/certs
+    openssl genrsa -out server.key 2048
+    openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
+    cd $CUR_DIR
+fi
+
 # 3072 bit key length
-CONTAINER_ID=$(docker run -d -p 8080:8080 -e OSSVC_KEYLENGTH=3072 -e OSSVC_TIMEOUTSEC=60 latanassov/ossvc:0.1.0)
+CONTAINER_ID=$(docker run -d -p 443:443 -e OSSVC_KEYLENGTH=3072 -v $CUR_DIR/certs:/certs latanassov/ossvc:0.1.0)
 
 go test -benchmem -run None github.com/LAtanassov/go-online-sphinx/pkg/client -bench .3072. -memprofile ./report/memprofile.out -cpuprofile ./report/profile.out
 
